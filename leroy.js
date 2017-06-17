07202dad4bc59cbd39eff6a2c2f36bd243adb080
Henning Erlandsen <henning.erlandsen@gmail.com>
2017-06-17 22:48:42 +0200

    Add jobs seeking

--- leroy.js
+++ leroy.js
@@ -1,6 +1,11 @@
-var http = require('http');
+const http = require('http');
+const jobs = require('./jobs')
 
 http.createServer(function (req, result) {
     result.writeHead(200, {'Content-Type': 'text/plain'});
-    result.end('Hello World!');
+    jobs.jobs('jobs').then((jobslist) => {
+    	result.end(jobslist);
+    }).catch((error) => {
+    	result.end('No jobs found in ./jobs directory')
+    });
 }).listen(8080);
