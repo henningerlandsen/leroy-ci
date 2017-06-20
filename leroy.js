27f60234081d22416d250792cfce1f77b96e2dd4
Henning Erlandsen <henning.erlandsen@gmail.com>
2017-06-20 20:59:50 +0200

    List the jobs in jobs folder

--- leroy.js
+++ leroy.js
@@ -1,11 +1,17 @@
 const http = require('http');
-const jobs = require('./jobs')
+const fs = require('fs');
+const util = require('util');
+
 
 http.createServer(function (req, result) {
     result.writeHead(200, {'Content-Type': 'text/plain'});
-    jobs.jobs('jobs').then((jobslist) => {
-    	result.end(jobslist);
+    
+    const readdir = util.promisify(fs.readdir);
+
+    readdir("jobs").then((jobslist) => {
+    	result.end(jobslist.join(','));
     }).catch((error) => {
+    	console.log('Error reading directory', error)
     	result.end('No jobs found in ./jobs directory')
     });
 }).listen(8080);
