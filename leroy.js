d527a0050c638f514d3cad26a8767f66ebbcfeda
Henning Erlandsen <henning.erlandsen@king.com>
2017-07-07 00:08:08 +0200

    Refactor tests and add correct disabled codes

--- leroy.js
+++ leroy.js
@@ -3,6 +3,9 @@ const util = require('util');
 const express = require('express')
 const app = express()
 
+function disabled(req, res) {
+    res.sendStatus(405);
+}
 
 app.get('/jobs', function (req, res) {
    const readdir = util.promisify(fs.readdir);
@@ -10,7 +13,13 @@ app.get('/jobs', function (req, res) {
     readdir("jobs").then((jobslist) => {
     	res.json(jobslist);
     });
-})
+});
+app.post('/jobs', disabled);
+app.put('/jobs', disabled);
+app.delete('/jobs', disabled);
+app.patch('/jobs', disabled);
+
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
--- test/api.js
+++ test/api.js
@@ -1,20 +1,54 @@
 var chakram = require('chakram');
 var expect = chakram.expect;
 
-describe('/jobs', function() {
-    describe('GET', function() {
-        it('returns a list of jobs', function() {
-            return chakram.get('http://localhost:8080/jobs').then(function(response) {
-                expect(response).to.have.status(200);
-                expect(response).to.be.json([ 'test.js' ]);
-            });
-        })
-    })
-    describe('POST', function() {
+
+var globals = {
+    server: '',
+    currentUrl: ''
+};
+
+function server(url, test) {
+    globals.server = url;
+    describe(url, test);
+}
+
+function endpoint(url, test) {
+    globals.currentUrl = globals.server + url;
+    describe(url, test);
+}
+
+function request(method, test) {
+    const m = method.toLowerCase();
+    return chakram[m](globals.currentUrl).then(test);
+}
+
+function disabled(method) {
+    describe(method, function() {
         it('is disabled', function() {
-            return chakram.post('http://localhost:8080/jobs').then(function(response) {
-                expect(response).to.have.status(404);
+            return request(method, function(response) {
+                expect(response).to.have.status(405);
             });
-        })
-    })
+        });
+    });
+}
+
+function result(method, expected_response) {
+    describe(method, function() {
+        it(`returns ${expected_response}`, function() {
+            return request(method, function(actual_res) {
+                expect(actual_res).to.have.status(200);
+                expect(actual_res).to.be.json(expected_response);
+            });
+        });
+    });
+}
+
+server('http://localhost:8080', () => {
+    endpoint('/jobs', () => {
+        result('GET', ['test.js']);
+        disabled('POST');
+        disabled('PUT');
+        disabled('PATCH');
+        disabled('DELETE');
+    });
 });
